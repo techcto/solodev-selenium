642096d89b88e3e15e8a2749f1ff4d5f332db7d5
ocoa <shawn@ocoa.com>
2018-12-04 17:43:05 -0500

    ..

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -14,7 +14,6 @@ def lambda_handler(event, context):
     if not notification_types:
         print("At least one CloudFormation notification type needs to be specified")
         return
-
     try:
         message = str(event["Records"][0]["Sns"]["Message"]).replace("\n", ",")
     except Exception:
@@ -29,16 +28,14 @@ def lambda_handler(event, context):
 
     for notification_type in notification_types:
         if notification_type in message:
-            continue
-
-        print(event)
-        print(context)
-
-        sns_subject = "CloudFormation %s" % (notification_type)
-        sns_message = message.replace(",", "\n")
-
-        if notification_type == "CREATE_COMPLETE":
-            dispatcher()
+            print(event)
+            print(context)
+            sns_subject = "CloudFormation %s" % (notification_type)
+            sns_message = message.replace(",", "\n")
+            if notification_type == "CREATE_COMPLETE":
+                dispatcher()
+        else:
+            return True
 
 
 def dispatcher():
