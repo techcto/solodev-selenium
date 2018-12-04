0ccd39a8590ba24c8f7ad06debadc58203a17303
ocoa <shawn@ocoa.com>
2018-12-04 14:38:46 -0500

    ..

--- package.sh
+++ package.sh
@@ -2,3 +2,5 @@ make build
 echo "Upload Lambda to S3"
 aws s3 cp build.zip s3://solodev-aws-ha/solodev-selenium.zip
 aws lambda update-function-code --function-name solodevSelenium --s3-bucket solodev-aws-ha --s3-key solodev-selenium.zip
+DATE=$(date +%d%H%M)
+aws codebuild start-build --project-name solodev-ami-builder --environment-variables-override name=SOLODEV_RELEASE,value=$DATE
