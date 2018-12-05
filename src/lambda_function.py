2a840c914f01e80f073c0cd2e05fb418b15e4288
ocoa <shawn@ocoa.com>
2018-12-04 21:17:40 -0500

    ..

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -38,10 +38,9 @@ def lambda_handler(event, context):
 
             if notification_type == "CREATE_COMPLETE":
                 dispatcher()
-            else
+            else:
                 echo "Wrong Type!"
-                return True
-        else
+        else:
             echo "Ignore!"
             return True
 
