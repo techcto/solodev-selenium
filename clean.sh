e5ff1e03a1b9da708c836298663f1d68a1845084
ocoa <shawn@ocoa.com>
2018-12-18 11:06:52 -0500

    update clean step

--- clean.sh
+++ clean.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 
-#echo $(aws cloudformation describe-stacks)
+echo $(aws cloudformation describe-stacks)
 for STACK in $(aws cloudformation describe-stacks --region us-east-1 | ./jq -r '.Stacks[].StackName')
 do
     if [[ $STACK == *"-selenium-"* ]]; then
         echo "I found one. Time to delete stack: ${STACK}.  Bye-Bye!"
         aws cloudformation delete-stack --stack-name $STACK
-    fi
-done
-
-#echo $(aws cloudformation describe-stacks)
-for STACK in $(aws cloudformation describe-stacks --region us-west-1 | ./jq -r '.Stacks[].StackName')
-do
-    if [[ $STACK == *"-selenium-"* ]]; then
-        echo "I found one. Time to delete stack: ${STACK}.  Bye-Bye!"
-        aws cloudformation delete-stack --region us-west-1 --stack-name $STACK
     fi
 done
