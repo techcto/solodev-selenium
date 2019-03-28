377d45b8668fd50f4c59c11e2bb3f2eb6e5f5923
ocoa <shawn@ocoa.com>
2019-03-28 10:18:49 -0400

    add .env support

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -107,4 +107,3 @@ def dispatcher(url, username, password, website_url):
 def _to_env(name):
     s1 = re.sub('(.)([A-Z][a-z]+)', r'\1_\2', name)
     return re.sub('([a-z0-9])([A-Z])', r'\1_\2', s1).upper()
-
