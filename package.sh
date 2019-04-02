5a3830a75ee44786330741a76f48c358ad6eb8fb
smmadara <smmadara@gmail.com>
2019-04-02 17:05:20 -0400

    Build pro

--- package.sh
+++ package.sh
@@ -4,9 +4,15 @@ echo "Upload Lambda to S3"
 aws s3 cp build.zip s3://solodev-aws-ha/solodev-selenium.zip
 aws lambda update-function-code --function-name solodevSelenium --s3-bucket solodev-aws-ha --s3-key solodev-selenium.zip
 DATE=$(date +%d%H%M)
-echo "Create Solodev Lite"
-echo $(aws s3 cp s3://build-secure/params/solodev-lite-single.json - ) > solodev-lite-single.json
-aws cloudformation create-stack --disable-rollback --stack-name lite-selenium-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
-    --parameters file://solodev-lite-single.json \
-    --template-url https://s3.amazonaws.com/solodev-aws-ha/aws/solodev-lite-linux.yaml \
-    --notification-arns ${NOTIFICATION_ARN}
+#echo "Create Solodev Lite"
+#echo $(aws s3 cp s3://build-secure/params/solodev-lite-single.json - ) > solodev-lite-single.json
+#aws cloudformation create-stack --disable-rollback --stack-name lite-selenium-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM \
+    #--parameters file://solodev-lite-single.json \
+    #--template-url https://s3.amazonaws.com/solodev-aws-ha/aws/solodev-lite-linux.yaml \
+    #--notification-arns ${NOTIFICATION_ARN}
+echo "Create Solodev Pro for Opsworks BYOL"
+echo $(aws s3 cp s3://build-secure/params/solodev-pro-single.json - ) > solodev-pro-single-byol.json
+aws cloudformation create-stack --disable-rollback --stack-name pro-byol-tmp-${DATE} --disable-rollback --capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM
+    --parameters file:///${CODEBUILD_SRC_DIR}/solodev-pro-single-byol.json
+    --template-url https://s3.amazonaws.com/solodev-aws-ha/aws/solodev-pro-opsworks-byol.yaml
+    --notification-arns $NOTIFICATION_ARN    
