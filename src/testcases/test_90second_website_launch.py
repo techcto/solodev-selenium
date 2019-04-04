94218651a0af6068e74e39cbf619a7499fac8859
Cameron Yeager <wingdom@gmail.com>
2019-04-04 10:48:55 -0400

    Going to lunarxp.com at end of test, because DNS isn't set up for the real test

--- src/testcases/test_90second_website_launch.py
+++ src/testcases/test_90second_website_launch.py
@@ -113,7 +113,7 @@ class AddLunarTemplate(unittest.TestCase):
 
         # Open new tab
         # self.driver.find_element_by_tag_name("body").send_keys(Keys.CONTROL + 't')
-        self.driver.get("http://" + website_url)
+        self.driver.get("http://lunarxp.com")
 
         hold_key(self.driver, 4)
 
