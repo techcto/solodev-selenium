14fb0f1c47cf385c557891f0101be59d7619151e
Cameron Yeager <wingdom@gmail.com>
2019-04-09 17:07:46 -0400

    Updated the template to show the new driver setup method and expand on how some of it works

--- src/testcases/test_case_template.py
+++ src/testcases/test_case_template.py
@@ -5,18 +5,28 @@ from src.pageobjects.home_page import HomePage
 from src.pageobjects.websites_page import WebsitePage
 from src.pageobjects.manage_website_page import ManageWebsitePage
 from src.helpers.utilities import Utilities
+from src.helpers.utilities import UtilNoDriver
 from selenium import webdriver
 
 
 class TestCaseTemplate(unittest.TestCase):
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.driver.maximize_window()
+
+    # This is where the driver setup _should_ go, but lambda/browserstack doesn't like it
+    # def setUp(self):
+    #    self.driver = webdriver
 
     # All tests must start with _test
     # url, user, pass must be included for login
     def test_case_template(self, url=strings.localhost_solodev_url,
-                           username=strings.username, password=strings.password):
+                                     username=strings.username, password=strings.password,
+                                     website_url=strings.sanity_page_url, browser_type=strings.default_browser_type):
+        # We have to declare the driver first
+        # It has to have browser type (firefox/chrome supported now)
+        # the URL to know if we're running aginst local or aws
+        # the "page load strategy" which defaults to "normal", so you con't actually need to put normal
+        util_no_driver = UtilNoDriver(self)
+        self.driver = util_no_driver.make_driver(browser_type, url, "normal")
+
         # Get the helper and page functions
         utilities = Utilities(self.driver)
         login_page = LoginPage(self.driver)
@@ -35,3 +45,13 @@ class TestCaseTemplate(unittest.TestCase):
         login_page.type_login(username, password)
         login_page.click_login()
 
+        # Assert true - some element is or isn't there, or some function that returns T/F
+        # The driver.quit is here for a safeguard, it shouldn't ever be called,
+        # But its unclear if browserstack obeys assert's properly
+        # This assert example makes sure the element is present by checking a list of matching elements is > 0
+        self.assertTrue(len(self.driver.find_elements_by_css_selector(".btn.btn-lg.btn-yellow")) > 0)
+        self.driver.quit()
+
+    # this should quit the driver properly after the assertion
+    def tearDown(self):
+        self.driver.quit()
