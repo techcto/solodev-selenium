f7f983cb912497387211798d9de0413e3587327d
techcto <shawn@ocoa.com>
2019-05-15 11:52:36 -0400

    add clean buckets

--- clean.sh
+++ clean.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+for BUCKET in $(aws s3api list-buckets --region us-east-1 | ./jq -r '.Buckets[].Name')
+do
+    if [[ $BUCKET == *"-selenium-"* ]]; then
+        echo "I found one. Time to delete a bucket: ${BUCKET}.  Bye-Bye!"
+        aws s3 rb s3://$BUCKET --force 
+    fi
+done
+
 echo $(aws cloudformation describe-stacks)
 for STACK in $(aws cloudformation describe-stacks --region us-east-1 | ./jq -r '.Stacks[].StackName')
 do
